7b5bb72542ecf6c4b959bcad5b82e68c9fca2714
Zas Mama <104ambrown@gmail.com>
2022-03-02 12:07:55 -0500

    created the station route and jsonified the query results

--- app.py
+++ app.py
@@ -43,21 +43,31 @@ def precipitation():
     """Query for the dates and temperature observations from the last year.
        Convert the query results to a Dictionary using date as the key and tobs as the value.
        Return the JSON representation of your dictionary."""
-    
     # Calculate the date 1 year ago from today (today being 2017-08-23)
     today = dt.date(2017, 8, 23)
     year_ago = today - dt.timedelta(days=365)
-
     # Perform a query to retrieve the date and precipitation scores
     results = session.query(Measurement.prcp, Measurement.date)\
                                         .filter(Measurement.date >= year_ago)\
                                         .filter(Measurement.date < today).all()
-
     # Creating a dictionary from the row data and appending it to a all_precipitation
     all_precipitation = []
     for precipitation in results:
         precipitation_dict = {}
         precipitation_dict[precipitation.date] = precipitation.prcp
         all_precipitation.append(precipitation_dict)
-
     return jsonify(all_precipitation)
+
+# Station route
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset."""
+    # Perform a query to retrieve the stations
+    results = session.query(Station.station).all()
+    # Create a dictionary from the row data and append to a list of all_stations
+    all_stations = []
+    for station in results:
+        stations_dict = {}
+        stations_dict["station"] = station.station
+        all_stations.append(stations_dict)
+    return jsonify(all_stations)
